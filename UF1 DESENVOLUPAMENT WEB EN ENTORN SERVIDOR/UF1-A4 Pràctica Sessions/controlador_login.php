678051282507daecc8056255416777d900ee2c6e
nmartos <72387198+nmartosg@users.noreply.github.com>
2020-10-28 16:34:42 +0100

    Update controlador_login.php

--- UF1 DESENVOLUPAMENT WEB EN ENTORN SERVIDOR/UF1-A4 Pràctica Sessions/controlador_login.php
+++ UF1 DESENVOLUPAMENT WEB EN ENTORN SERVIDOR/UF1-A4 Pràctica Sessions/controlador_login.php
@@ -14,20 +14,28 @@ $_SESSION["errorcontra"]="";
     $registre = ['email' => 'noelia@gmail.com', 'password' => password_hash('123', PASSWORD_BCRYPT)];
                
     // Variables
-    $email = isset($_REQUEST['email']) ? $_REQUEST['email'] : null;
-    $contrasenya = isset($_REQUEST['contrasenya']) ? $_REQUEST['contrasenya'] : null;
+    $email = test_input($_REQUEST['email']) ? $_REQUEST['email'] : null;
+    $contrasenya = test_input($_REQUEST['contrasenya']) ? $_REQUEST['contrasenya'] : null;
+    //Validació dels camps
+    $_SESSION["email"]=test_input($_REQUEST["email"]);
+        $_SESSION["contrasenya"]=test_input($_REQUEST["contrasenya"]);
 
+        if (!filter_var($_SESSION["email"], FILTER_VALIDATE_EMAIL)) {
+            $_SESSION["errormail"]="Correo no vàlid.";
+        }else{
+            $_SESSION["errormail"]="";
+        }
+        if (!preg_match("/^[a-zA-Z1-9' ]*$/",$_SESSION["contrasenya"])) {
+            $_SESSION["errorcontra"]= " Només es permeten contrasenyas amb números i lletres.";
+        }else{
+            $_SESSION["errorcontra"]="";
+        }
+        
     // Comprovem si les dades del registre són correctes.
     if ($registre['email'] == $email && password_verify($contrasenya, $registre['password'])) {
         // Si són correctes, creeem la sessió
         session_start();
         //Validació dels camps 
-        if (!filter_var($_SESSION["email"], FILTER_VALIDATE_EMAIL)) {
-            $_SESSION["errormail"]="Correo no vàlid.";
-        }
-        if (!preg_match("/^[a-zA-Z1-9' ]*$/",$_SESSION["contrasenya"])) {
-            $_SESSION["errorcontra"]= "només es permeten contrasenyas amb números i lletras.";
-        }
         
         $_SESSION['email'] = $_REQUEST['email'];
         // I redireccionem a la pàgina privada.
