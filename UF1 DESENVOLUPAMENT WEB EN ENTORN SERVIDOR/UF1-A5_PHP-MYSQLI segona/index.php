052c46fb9d659a3a5ace6c8cd2f40a0d3c688b15
nmartos <72387198+nmartosg@users.noreply.github.com>
2020-11-15 19:51:36 +0100

    a5 php-mysqli

---
+++ UF1 DESENVOLUPAMENT WEB EN ENTORN SERVIDOR/UF1-A5_PHP-MYSQLI segona/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Practica A05_NoeliaMartos</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+	<h1>LOGIN / REGISTRE USUARI</h1><br>
+	<div>
+		<h3>LOGIN USUARI</h3>
+		<p>Si ja estas registrat <a href="publica.php" title="Login">clica aqui!</a></p><hr /><br>
+	</div>
+	<div>
+		<h3>REGISTRE USUARI</h3>
+		<p>Si et vols registrar <a href="register.php" title="Registre">clica aqui!</a></p><hr /><br>
+	</form>		
+	</div>		
+	</body>
+</html>
