c94179374bd030d569a80155638d3c167a9eb758
nmartos <72387198+nmartosg@users.noreply.github.com>
2020-11-16 12:13:06 +0100

    Update index.php

--- UF1 DESENVOLUPAMENT WEB EN ENTORN SERVIDOR/UF1-A6-Gestio de productes/index.php
+++ UF1 DESENVOLUPAMENT WEB EN ENTORN SERVIDOR/UF1-A6-Gestio de productes/index.php
@@ -11,7 +11,7 @@ session_start();
 	<meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
-	<h1>LOGIN / REGISTRE USUARI</h1><br>
+	<h1>LOGIN / REGISTRE USUARI / PRODUCTES</h1><br>
 	<div>
 		<h3>LOGIN USUARI</h3>
 		<p>Si ja estas registrat <a href="publica.php" title="Login">clica aqui!</a></p><hr /><br>
